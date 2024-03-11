0b3f8f93b61e851bc000240ed72678f59d474b85
MeyZo0 <vgadstrev@gmail.com>
2024-03-11 13:52:05 +0200

    new project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IPZ_CW_R1_Syshchuk_Vladislav"
+include(":app")
+ 
